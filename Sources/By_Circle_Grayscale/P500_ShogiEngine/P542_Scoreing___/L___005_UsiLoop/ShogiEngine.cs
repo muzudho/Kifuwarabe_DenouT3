339461b4956a105e1755fdd677d78fa7914e5636
muzudho <muzudho1@gmail.com>
2016-04-11 19:40:28 +0900

    とにかく全部ほおりこめだぜ☆！（＾ｑ＾）！

---
+++ Sources/By_Circle_Grayscale/P500_ShogiEngine/P542_Scoreing___/L___005_UsiLoop/ShogiEngine.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace Grayscale.P542_Scoreing___.L___005_UsiLoop
+{
+    public interface ShogiEngine
+    {
+        /// <summary>
+        /// 送信
+        /// </summary>
+        /// <param name="line">メッセージ</param>
+        void Send(string line);
+
+
+        /// <summary>
+        /// きふわらべの作者名です。
+        /// </summary>
+        string AuthorName { get; }
+
+        /// <summary>
+        /// 製品名です。
+        /// </summary>
+        string SeihinName { get; }
+
+        /// <summary>
+        /// USI「setoption」コマンドのリストです。
+        /// </summary>
+        Dictionary<string, string> SetoptionDictionary { get; set; }
+
+        /// <summary>
+        /// USI「ponder」の使用の有無です。
+        /// </summary>
+        bool Option_enable_usiPonder { get; set; }
+
+        /// <summary>
+        /// サーバーに「noop」コマンドを投げてもよいかどうかです。
+        /// サーバーに「noop」を送ると、「ok」を返してくれるものとします。１分間を空けてください。
+        /// </summary>
+        bool Option_enable_serverNoopable { get; set; }
+
+    }
+}
