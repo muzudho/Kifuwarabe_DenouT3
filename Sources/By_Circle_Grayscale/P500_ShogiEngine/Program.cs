34062528c5ad18a5c05c15507b77188f38170c19
むずでょ <muzudho1@gmail.com>
2020-11-14 16:41:25 +0900

    動かねー☆（＾～＾）

--- Sources/By_Circle_Grayscale/P500_ShogiEngine/Program.cs
+++ Sources/By_Circle_Grayscale/P500_ShogiEngine/Program.cs
@@ -1,4 +1,5 @@
-﻿using Grayscale.P003_Log________.L___500_Struct;
+﻿using System;
+using Grayscale.P003_Log________.L___500_Struct;
 using Grayscale.P003_Log________.L500____Struct;
 using Grayscale.P571_KifuWarabe_.L500____KifuWarabe;
 
