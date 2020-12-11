b514ba77ef7939c9578e80669594c59974b3789b
むずでょ <muzudho1@gmail.com>
2020-12-12 04:59:01 +0900

    Restructure source directories.

---
+++ Sources/P150_LogKyokuPng/TestProgram.cs
@@ -0,0 +1,140 @@
+﻿using Grayscale.P157KyokumenPng.I500Struct;
+using Grayscale.P157KyokumenPng.L500Struct;
+using Grayscale.P158LogKyokuPng.L500UtilWriter;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using Nett;
+
+namespace Grayscale.P159_Form_______
+{
+    static class TestProgram
+    {
+
+        static void AppendCommandline(Dictionary<string, string> dic)
+        {
+            string[] args = Environment.GetCommandLineArgs();
+
+            foreach(string arg in args)
+            {
+                string name;
+                string value;
+
+                string rest = arg;
+                if (!rest.StartsWith("--"))
+                {
+                    goto gt_Next1;
+                }
+
+                rest = rest.Substring(2);
+
+                int eq = rest.IndexOf('=');
+                if (-1 == eq)
+                {
+                    goto gt_Next1;
+                }
+
+                name = rest.Substring(0, eq).Trim();
+                value = rest.Substring(eq + 1).Trim();
+
+                if (value.StartsWith("\"") && value.EndsWith("\""))
+                {
+                    value = value.Substring(1, value.Length - 2);
+                }
+
+                if (dic.ContainsKey(name))
+                {
+                    dic[name] = value;
+                }
+                else
+                {
+                    dic.Add(name, value);
+                }
+
+            gt_Next1:
+                ;
+            }
+        }
+
+
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void TestMain()
+        {
+            var profilePath = System.Configuration.ConfigurationManager.AppSettings["Profile"];
+            var toml = Toml.ReadFile(Path.Combine(profilePath, "Engine.toml"));
+
+            //
+            // コマンドライン引数の例
+            //
+            // --position="position sfen 1nsgkgsnl/9/p2pppppp/9/9/9/P2PPPPPP/1B5R1/1NSGKGSNL w L2Pl2p 1 moves 5a6b 7g7f 3a3b" \
+            // --outFolder="../../Logs/"
+            // --outFile="_log_局面1.png"
+            // --imgFolder="../../Data/img/gkLog/" \
+            // --kmFile="koma1.png" \
+            // --sjFile="suji1.png" \
+            // --kmW=20 \
+            // --kmH=20 \
+            // --sjW=8 \
+            // --sjH=12 \
+            // --end
+            //
+
+            // ヌル防止のための初期値
+            Dictionary<string, string> argsDic = new Dictionary<string, string>();
+            argsDic.Add("position", "position startpos moves");
+            argsDic.Add("outFolder", "./");//出力フォルダー "../../Logs/"
+            argsDic.Add("outFile", Path.Combine(profilePath, toml.Get<TomlTable>("Resources").Get<string>("N1PngBasename")));//出力ファイル
+            argsDic.Add("imgFolder", ".");//画像フォルダーへのパス image path
+            argsDic.Add("kmFile", Path.Combine(profilePath, toml.Get<TomlTable>("Resources").Get<string>("N2PngBasename")));//駒画像へのパス。
+            argsDic.Add("kmW", "1");//駒の横幅。koma width
+            argsDic.Add("kmH", "1");
+            argsDic.Add("sjFile", Path.Combine(profilePath, toml.Get<TomlTable>("Resources").Get<string>("N3PngBasename")));//数字・小
+            argsDic.Add("sjW", "1");//数字の横幅。suji width
+            argsDic.Add("sjH", "1");
+            TestProgram.AppendCommandline(argsDic);
+
+            //foreach (KeyValuePair<string, string> entry in argsDic)
+            //{
+            //    MessageBox.Show("["+entry.Key + "]=[" + entry.Value+"]", "デバッグ");
+            //}
+            //MessageBox.Show("出力先=[" + Path.Combine(Application.StartupPath, argsDic["outPath"]) + "]", "デバッグ");
+
+
+
+            //
+            // SFEN
+            //
+            string sfen;
+            {
+                // SFEN を分解したい。
+                //string sfen = "lnsgkgsn1/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL w - 1";
+                //string sfen = "position sfen lnsgkgsnl/9/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL w - 1 moves 5a6b 7g7f 3a3b";
+                //string sfen = "position sfen lnsgkgsnl/9/p1ppppppp/9/9/9/P1PPPPPPP/1B5R1/LNSGKGSNL w Pp 1 moves 5a6b 7g7f 3a3b";
+                //string sfen = "position sfen 1nsgkgsnl/9/p2pppppp/9/9/9/P2PPPPPP/1B5R1/1NSGKGSNL w L2Pl2p 1 moves 5a6b 7g7f 3a3b";
+                sfen = argsDic["position"];
+            }
+
+            KyokumenPngEnvironment reportEnvironment = new KyokumenPngEnvironmentImpl(
+                    argsDic["outFolder"],
+                    argsDic["imgFolder"],
+                    argsDic["kmFile"],
+                    argsDic["sjFile"],
+                    argsDic["kmW"],
+                    argsDic["kmH"],
+                    argsDic["sjW"],
+                    argsDic["sjH"]
+                );
+            // テスト・プログラム
+            Util_KyokumenPng_Writer.Write_ForTest(
+                sfen,
+                "",
+                argsDic["outFile"],
+                reportEnvironment
+                );
+
+        }
+    }
+}
---
+++ Sources/P720_FvWriter___/Uc_Main.cs
@@ -0,0 +1,207 @@
+﻿using Grayscale.P003Log.I500Struct;
+using Grayscale.P003Log.L500Struct;
+using Grayscale.P027Settei.L500Struct;
+using Grayscale.P163KifuCsa.I250Struct;
+using Grayscale.P163KifuCsa.L250Struct;
+using Grayscale.P521FeatureVect.I500Struct;
+using Grayscale.P521FeatureVect.L500Struct;
+using Grayscale.P523UtilFv.L480UtilFvEdit;
+using Grayscale.P523UtilFv.L490UtilFvFormat;
+using Grayscale.P523UtilFv.L491UtilFvIo;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using Nett;
+
+namespace Grayscale.P720_FvWriter___
+{
+    public partial class Uc_Main : UserControl
+    {
+
+        public Uc_Main()
+        {
+            InitializeComponent();
+        }
+
+        private void btnWriter_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void btnMakeRandom_Click(object sender, EventArgs e)
+        {
+            var profilePath = System.Configuration.ConfigurationManager.AppSettings["Profile"];
+            var toml = Toml.ReadFile(Path.Combine(profilePath, "Engine.toml"));
+            string filepath = Path.Combine(profilePath, toml.Get<TomlTable>("Resources").Get<string>("Fv00KomawariSample"));
+
+            FeatureVector fv = new FeatureVectorImpl();
+            Util_FeatureVectorEdit.Make_Random(fv);
+
+            File.WriteAllText(filepath, Format_FeatureVector_Komawari.Format_Text(fv));
+            MessageBox.Show("サンプルファイルを書き出しました。\n" +
+                "filepath=[" + filepath + "]");
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            var profilePath = System.Configuration.ConfigurationManager.AppSettings["Profile"];
+            var toml = Toml.ReadFile(Path.Combine(profilePath, "Engine.toml"));
+
+            IErrorController errH = ErrorControllerReference.TestProgram;
+
+            string filepathR = Path.Combine(profilePath, toml.Get<TomlTable>("Resources").Get<string>("Fv00Komawari"));
+            string filepathR_KK = Path.Combine(profilePath, toml.Get<TomlTable>("Resources").Get<string>("Fv01KK"));
+            string filepathW = Path.Combine(profilePath, toml.Get<TomlTable>("Resources").Get<string>("Fv2Sample"));
+
+            FeatureVector fv = new FeatureVectorImpl();
+
+            if (Util_FeatureVectorInput.Make_FromFile_Komawari(fv, filepathR))
+            {
+            }
+
+            if (Util_FeatureVectorInput.Make_FromFile_KK(fv, filepathR_KK, errH))
+            {
+            }
+
+            File.WriteAllText(filepathW, Format_FeatureVector_Komawari.Format_Text(fv));
+
+#if DEBUG
+            MessageBox.Show("FVファイルを読み込んで、書き出しました。\n" +
+                "readFilepath=["+filepathR+"]"+
+                "writeFilepath=[" + filepathW + "]");
+#endif
+        }
+
+        /// <summary>
+        /// 棋譜読込み
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnReadKifu_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(this.txtKifuFilepath.Text))
+            {
+                goto gt_EndMethod;
+            }
+
+
+            CsaKifu csaKifu = Util_Csa.ReadFile(this.txtKifuFilepath.Text);
+
+            StringBuilder sb = new StringBuilder();
+            List<CsaKifuSasite> sasiteList = csaKifu.SasiteList;
+            foreach(CsaKifuSasite csaSasite in sasiteList)
+            {
+                sb.Append(csaSasite.OptionTemezumi);
+                sb.Append("手目 ");
+                sb.Append(csaSasite.DestinationMasu);
+                sb.Append(" ");
+                sb.Append(csaSasite.Second);
+                sb.Append(" ");
+                sb.Append(csaSasite.Sengo);
+                sb.Append(" ");
+                sb.Append(csaSasite.SourceMasu);
+                sb.Append(" ");
+                sb.Append(csaSasite.Syurui);
+                sb.AppendLine();
+            }
+            this.txtSasiteList.Text = sb.ToString();
+
+        gt_EndMethod:
+            ;
+        }
+
+        /// <summary>
+        /// 「ゼロクリアーでファイル作成」（タイアド・ベクター）
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnMakeZero_Tv_Click(object sender, EventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// リザルト・ベクターを０クリアーで作成
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCreateRv0Clear_Click(object sender, EventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// 表変形KK 書き出し（旧Fvから）
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_HyoHenkeiFvKK_Click(object sender, EventArgs e)
+        {
+            var profilePath = System.Configuration.ConfigurationManager.AppSettings["Profile"];
+            var toml = Toml.ReadFile(Path.Combine(profilePath, "Engine.toml"));
+            string dataDirectory = Path.Combine(profilePath, toml.Get<TomlTable>("Resources").Get<string>("DataDirectory"));
+
+            FeatureVector fv = new FeatureVectorImpl();
+            Util_FeatureVectorInput.Make_FromFile_Komawari(fv, Path.Combine(profilePath, toml.Get<TomlTable>("Resources").Get<string>("Fv00Komawari")));
+
+            Util_FeatureVectorOutput.Write_KK(fv, Path.Combine(profilePath, dataDirectory));
+        }
+
+        /// <summary>
+        /// 1P玉KP書出しボタン。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_1pKP_Write_Click(object sender, EventArgs e)
+        {
+            var profilePath = System.Configuration.ConfigurationManager.AppSettings["Profile"];
+            var toml = Toml.ReadFile(Path.Combine(profilePath, "Engine.toml"));
+            string dataDirectory = Path.Combine(profilePath, toml.Get<TomlTable>("Resources").Get<string>("DataDirectory"));
+
+            FeatureVector fv = new FeatureVectorImpl();
+            Util_FeatureVectorInput.Make_FromFile_Komawari(fv, Path.Combine(profilePath, toml.Get<TomlTable>("Resources").Get<string>("Fv00Komawari")));
+
+            Util_FeatureVectorOutput.Write_KP(fv, Path.Combine(profilePath, dataDirectory));
+        }
+
+        /// <summary>
+        /// [PP書出し]ボタン。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnWriteFvPp_Click(object sender, EventArgs e)
+        {
+            var profilePath = System.Configuration.ConfigurationManager.AppSettings["Profile"];
+            var toml = Toml.ReadFile(Path.Combine(profilePath, "Engine.toml"));
+            string dataDirectory = Path.Combine(profilePath, toml.Get<TomlTable>("Resources").Get<string>("DataDirectory"));
+
+            FeatureVector fv = new FeatureVectorImpl();
+            Util_FeatureVectorInput.Make_FromFile_Komawari(fv, Path.Combine(profilePath, toml.Get<TomlTable>("Resources").Get<string>("Fv00Komawari")));
+
+            Util_FeatureVectorOutput.Write_PP_Banjo(fv, Path.Combine(profilePath, dataDirectory));
+            Util_FeatureVectorOutput.Write_PP_19Mai(fv, Path.Combine(profilePath, dataDirectory));
+            Util_FeatureVectorOutput.Write_PP_5Mai(fv, Path.Combine(profilePath, dataDirectory));
+            Util_FeatureVectorOutput.Write_PP_3Mai(fv, Path.Combine(profilePath, dataDirectory));
+        }
+
+        /// <summary>
+        /// fv_00_Scale.csv書き出し。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnWriteFvScale_Click(object sender, EventArgs e)
+        {
+            var profilePath = System.Configuration.ConfigurationManager.AppSettings["Profile"];
+            var toml = Toml.ReadFile(Path.Combine(profilePath, "Engine.toml"));
+
+            string filepathW = Path.Combine( Application.StartupPath, Path.Combine(profilePath, toml.Get<TomlTable>("Resources").Get<string>("Fv00Scale")));
+            MessageBox.Show("filepathW=[" + filepathW + "]", "fv_00_Scale.csv書き出し。");
+
+            FeatureVector fv = new FeatureVectorImpl();
+            fv.SetBairitu_NikomaKankeiPp( 0.002f);//仮の初期値。
+
+            File.WriteAllText(filepathW, Format_FeatureVector_Scale.Format_Text(fv));
+        }
+
+
+    }
+}
